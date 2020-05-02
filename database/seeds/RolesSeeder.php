54a258ceac5f005cab04b0818fc5a2a510f4f379
sorafia <siafosofia@hotmail.com>
2020-05-03 00:31:57 +0700

    สร้าง role

---
+++ database/seeds/RolesSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->insert([
+            ['name' => 'Administrator',
+                'slug' => 'admin',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')],
+            ['name' => 'Member',
+                'slug' => 'member',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')],
+        ]);
+    }
+}
